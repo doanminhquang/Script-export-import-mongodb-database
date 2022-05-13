51cfef48581ea1921aba9dd1cd07c3770f5d92d6
Đoàn Minh Quang <42968935+doanminhquang@users.noreply.github.com>
2022-05-13 22:59:08 +0700

    Update db_tool.py

--- db_tool.py
+++ db_tool.py
@@ -37,7 +37,7 @@ def mongoimport(database_name,collection_name,path):
 
 def run_single(choice_mode,database_name,collection_name):
     try:
-        print("Choice: "+collection_name)
+        print("***** Choice: "+collection_name)
         if(choice_mode == switcher_mode[0]):
             path_output = global_export+database_name+'/'+collection_name+'.json'
             mongoexport(database_name,collection_name,path_output)
@@ -65,37 +65,37 @@ if __name__ == "__main__":
     switcher_mode={0:'Export', 1:'Import', 2:'Import And Create'}
 
     for j in range(len(switcher_mode)):
-        print("\t"+str(j)+" : "+switcher_mode[j])
+        print(str(j)+" : "+switcher_mode[j])
 
     min = 0
     max = len(switcher_mode)-1
-    n = input_int("Enter selection ("+str(min)+"-"+str(max)+"): ",min,max)
+    n = input_int("----- Enter selection ("+str(min)+"-"+str(max)+"): ",min,max)
 
     choice_mode = switcher_mode.get(n,"Invalid choice")
-    print("Choice: "+choice_mode+" mode")  
+    print("***** Choice: "+choice_mode+" mode")  
 
     cmd = "cd " + pathmongodb
     returned_value = os.system(cmd) 
 
     dbs = client.list_database_names()
     for j in range(len(dbs)):
-        print("\t"+str(j)+" : "+dbs[j])
+        print(str(j)+" : "+dbs[j])
 
     if choice_mode != switcher_mode[2]:
         min = 0
         max = len(dbs)-1
-        m = input_int("Index of db name ("+str(min)+"-"+str(max)+"): ",min,max)
+        m = input_int("----- Enter index of db name ("+str(min)+"-"+str(max)+"): ",min,max)
     else:
         min = -1
         max = len(dbs)-1
-        m = input_int("Index of db name ("+str(min)+" = create new) || ("+str(min+1)+"-"+str(max)+"): ",min,max)
+        m = input_int("----- Enter index of db name ("+str(min)+" = create new) || ("+str(min+1)+"-"+str(max)+"): ",min,max)
 
     if m != -1 :
         database_name = dbs[m]
     else:
-        database_name = input("Enter db name: ")
+        database_name = input("----- Enter db name: ")
     
-    print("Choice: "+database_name)
+    print("***** Choice: "+database_name)
     
     if choice_mode != switcher_mode[2]:
         collections = client[database_name].list_collection_names()
@@ -105,13 +105,16 @@ if __name__ == "__main__":
         collections = []
         for filename in os.listdir(global_import):
             if filename.endswith(".json"):
-                collections.append(os.path.splitext(filename)[0])
+                collections.append(os.path.splitext(filename)[0])        
+        if len(collections) == 0 :
+            print("File import not found")
+            raise SystemExit
         min = -1
         max = len(collections)-1
-    
+
     for j in range(len(collections)):
-        print("\t"+str(j)+" : "+collections[j])
-    p = input_int("Index of collection name ("+str(min)+" = all) || ("+str(min+1)+"-"+str(max)+"): ",min,max)
+        print(str(j)+" : "+collections[j])
+    p = input_int("----- Enter index of collection name ("+str(min)+" = all) || ("+str(min+1)+"-"+str(max)+"): ",min,max)
 
     if(p==-1):
         run_all(choice_mode,database_name,collections)
