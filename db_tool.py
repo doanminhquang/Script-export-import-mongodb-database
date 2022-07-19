7c2fcaa13d0560d679d22b70dc50d3dd884a3783
Đoàn Minh Quang <42968935+doanminhquang@users.noreply.github.com>
2022-07-19 18:32:21 +0700

    Update db_tool.py

--- db_tool.py
+++ db_tool.py
@@ -28,11 +28,11 @@ def input_int(str,min,max):
             return n
 
 def mongoexport(database_name,collection_name,path):
-    cmd = "mongoexport --db "+database_name+" --collection "+collection_name+" --out "+path
+    cmd = "mongoexport --db " + database_name + " --collection " + collection_name + " --out "+path
     os.system(cmd)     
 
 def mongoimport(database_name,collection_name,path):
-    cmd = "mongoimport --db "+database_name+" --collection "+collection_name+" --file "+path
+    cmd = "mongoimport --db " + database_name + " --collection " + collection_name + " --file "+path
     os.system(cmd) 
 
 def run_single(choice_mode,database_name,collection_name):
