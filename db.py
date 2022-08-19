5fdfe0b302eb0cfd4d3ed6f7cea9f3281f86204a
Đoàn Minh Quang <42968935+doanminhquang@users.noreply.github.com>
2022-08-19 14:27:16 +0700

    Update db.py

--- db.py
+++ db.py
@@ -40,7 +40,7 @@ def run_single(choice_mode,database_name,collection_name):
         print("***** Choice: "+collection_name)
         if(choice_mode == switcher_mode[0]):
             path_output = global_export+database_name+'/'+collection_name+'.json'
-            mongoexport(database_name,collection_name,path_output)
+            mongoexport(database_name , collection_name,path_output)
         else:
             path_input = global_import + collection_name + '.json'
             mongoimport(database_name, collection_name, path_input)  
