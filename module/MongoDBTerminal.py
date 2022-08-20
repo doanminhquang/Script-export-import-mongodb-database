e52d5a0e23cdad8d73b46a2ef19ab4de3fd84b3e
Đoàn Minh Quang <42968935+doanminhquang@users.noreply.github.com>
2022-08-20 10:41:16 +0700

    Add files via upload

---
+++ module/MongoDBTerminal.py
@@ -0,0 +1,13 @@
+from os import system
+
+def GoToMongoDB(path):
+    cmd = "cd " + path
+    system(cmd) 
+
+def MongoExport(database_name, collection_name, path):
+    cmd = "mongoexport --db %s --collection %s --out %s" % (database_name, collection_name, path)
+    system(cmd)     
+
+def MongoImport(database_name, collection_name, path):
+    cmd = "mongoimport --db %s --collection %s --file %s" % (database_name, collection_name, path)
+    system(cmd) 
